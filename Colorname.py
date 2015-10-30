09337b83c66ce295b776b4acdeb90f6dec85d56c
Marcel Simon <marcel.simon@uni-jena.de>
2015-10-30 17:22:10 +0100

    Adapted colorname for gray valued images

--- Colorname.py
+++ Colorname.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 from numpy.ma import squeeze
 from IAlgorithm import IAlgorithm
 from Blob import Blob
@@ -11,13 +11,19 @@ __author__ = 'simon'
 class Colorname(IAlgorithm):
     def __init__(self):
         self.mapping = genfromtxt('data/colormapping.txt', delimiter=',')[:,3:]
+        self.init_threading()
 
     def _compute(self, blob_generator):
         # Design pattern:
         for blob in blob_generator:
             # Map all RGB values to colorname histograms
-
-            r,g,b=split((blob.data.astype(numpy.uint8)/8).astype(numpy.uint32),3,axis=2)
+            if len(blob.data.shape)==2:
+                blob.data = (blob.data.astype(np.uint8)/8).astype(np.uint32)
+                r=blob.data
+                g=blob.data
+                b=blob.data
+            else:
+                r,g,b=split((blob.data.astype(np.uint8)/8).astype(np.uint32),3,axis=2)
             index = r+32*g+32*32*b
             blob.data = self.mapping[squeeze(index)]
             yield blob
