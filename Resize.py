b29688cc493bc435e8bb0081aebf8def275f1b6f
Marcel Simon <marcel.simon@uni-jena.de>
2015-10-30 17:23:19 +0100

    Changed resize algorithm

--- Resize.py
+++ Resize.py
@@ -1,5 +1,6 @@
 import logging
 from scipy.misc import imresize
+from skimage.transform import resize
 from IAlgorithm import IAlgorithm
 
 __author__ = 'simon'
@@ -9,8 +10,9 @@ class Resize(IAlgorithm):
     def __init__(self, width, height):
         self.width = width
         self.height = height
+        #self.init_threading()
 
     def _compute(self, blob_generator):
         for blob in blob_generator:
-            blob.data = imresize(blob.data,(self.height,self.width))
+            blob.data = resize(blob.data,(self.height,self.width),1)
             yield blob
