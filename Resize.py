0897b283764cceb2d0d4e8733dc684a15c7e16af
Marcel Simon <marcel.simon@uni-jena.de>
2016-10-07 11:32:35 +0200

    Added error to resize, if mode stretch is unsed and only 1d shape is passed

--- Resize.py
+++ Resize.py
@@ -11,10 +11,15 @@ class Resize(IAlgorithm):
     def __init__(self, target_shape, mode='stretch'):
         self.target_shape = np.array(target_shape).astype(np.float64)
         self.mode = mode
+        
+        if mode == 'stretch':
+            assert self.target_shape.size >= 2, 'Mode "stretch" requires the target shape to be at least 2 dimensional.'
+            
         if mode in ['resize_larger_side','resize_smaller_side']:
             assert isinstance(target_shape,int) or len(target_shape)==1, 'Please provide only a single shape value to Resize if using the mode "resize_{smaller,larger}_side'
             if isinstance(target_shape,int):
                 self.target_shape = (self.target_shape, )
+                
     def _compute(self, blob_generator):
         for blob in blob_generator:
             if self.mode == 'stretch':
